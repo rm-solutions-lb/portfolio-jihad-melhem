9cb231b9f813edfd88d16239f53750aca60d4ef0
Roger Moughames <roger.moughames@grappasystems.com>
2024-10-24 13:31:27 +0300

    Update

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/*===== typed js =====*/
+
+const typed = new Typed(".multiple-text", {
+    strings: [
+      "UI/UX Designer!",
+      "Interior Designer!",
+    ],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 500,
+    loop: true,
+  });
