23bd0f1f8c33921de7771b63f0deb618b562e957
Roger Moughames <roger.moughames@grappasystems.com>
2024-11-05 18:54:50 +0200

    Update

--- js/script.js
+++ js/script.js
@@ -10,3 +10,29 @@ const typed = new Typed(".multiple-text", {
     backDelay: 500,
     loop: true,
   });
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+  sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+
+    if(top >= offset && top < offset + height) {
+      navLinks.forEach(links => {
+        links.classList.remove('active');
+        document.querySelector('header nav a[href*=' + id + ' ]').classList.add('active');
+      })
+    }
+  })
+}
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
+}
